8af3858e684e68b42618f8a985ff6ddaaf818fde
Pavel Lankmiler <pavel@incorporatenow.com>
2020-02-14 16:18:56 +0200

    Testing server

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 require_once 'validator.php';
 
+
+echo '<pre>';
+print_r($_SERVER['HTTP_REFERER']);
+die();
+
 if(isset($_GET['key']) && isset($_GET['email'])) {
     $validator = new Validator($_GET['key']);
     $validator->validate($_GET['email']);
