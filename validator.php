84b9606ba471a2d499dd4ff6948142835863a2e4
Pavel Lankmiler <pavel@incorporatenow.com>
2020-02-14 15:50:42 +0200

    Before call reverted

--- validator.php
+++ validator.php
@@ -15,28 +15,24 @@ class Validator {
         $this->auth_code = $auth_code;
         $this->valid_domains = $valid_domains;
     }
-
-    /*
-     * Magic method that is using "beforeCall" to restrict if there are
-     * invalid domains or auth code
-     */
-    public function __call($method, $arguments) {
-        die();
-        if(method_exists($this, $method)) {
-            $this->beforeCall();
-            return call_user_func_array(array($this,$method),$arguments);
-        }
-    }
+//
+//    /*
+//     * Magic method that is using "beforeCall" to restrict if there are
+//     * invalid domains or auth code
+//     */
+//    public function __call($method, $arguments) {
+//        die();
+//        if(method_exists($this, $method)) {
+//            $this->beforeCall();
+//            return call_user_func_array(array($this,$method),$arguments);
+//        }
+//    }
 
     /*
      * Will be called before any method
      */
     function beforeCall()
     {
-        print_r($this->auth_code);
-        print_r(self::$valid_auth_code);
-        die();
-
         if(!in_array(parse_url($_SERVER['HTTP_REFERER']), $this->valid_domains) || $this->auth_code != self::$valid_auth_code) {
             header('HTTP/1.0 403 Forbidden');
             exit();
@@ -48,6 +44,7 @@ class Validator {
      */
     function validate($email)
     {
+        $this->beforeCall();
         $validator = EmailValidation\EmailValidatorFactory::create($email);
         header('Content-Type: application/json');
         echo json_encode($validator->getValidationResults()->asJson());
